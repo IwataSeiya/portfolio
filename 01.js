fec8e9494641e926589265c1cd9de6386819a612
IwataSeiya <107803212+IwataSeiya@users.noreply.github.com>
2022-07-22 13:55:06 +0900

    Update 01.js

--- 01.js
+++ 01.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
   "particles":{
     "number":{
-      "value":1046,//この数値を変更すると星の数が増減できる
+      "value":1046,
       "density":{
         "enable":true,
         "value_area":900
@@ -11,19 +11,19 @@ particlesJS("particles-js", {
       "value":"#ffffff"
     },
     "shape":{
-      "type":"circle",//形状はcircleを指定
+      "type":"circle",
       "stroke":{
         "width":0
       },
       },
     "opacity":{
-      "value":1,//シェイプの透明度
-      "random":true,//シェイプの透明度をランダムにする
+      "value":1,
+      "random":true,
       "anim":{
-        "enable":true,//シェイプの透明度をアニメーションさせる
-        "speed":3,//シェイプの透明度をアニメーションさせる
-        "opacity_min":0,//透明度の最小値０
-        "sync":false//全てを同時にアニメーションさせない
+        "enable":true,
+        "speed":3,
+        "opacity_min":0,
+        "sync":false
       }
     },
     "size":{
@@ -41,10 +41,10 @@ particlesJS("particles-js", {
     },
     "move":{
       "enable":true,
-      "speed":120,//この数値を小さくするとゆっくりな動きになる
-    "direction":"none",//方向指定なし
-    "random":true,//動きはランダムに
-    "straight":true,//動きをとどめる
+      "speed":120,
+    "direction":"none",
+    "random":true,
+    "straight":true,
       "out_mode":"out",
       "bounce":false,
       "attract":{
@@ -93,32 +93,33 @@ window.addEventListener("load", function() {
   });
 
   const bar = new ProgressBar.Line(load_text, {
-    easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
-    duration: 2000,//時間指定(1000＝1秒)
-    strokeWidth: 0.2,//進捗ゲージの太さ
-    color: '#FF0',//進捗ゲージのカラー
-    trailWidth: 0.2,//ゲージベースの線の太さ
-    trailColor: '#FFF',//ゲージベースの線のカラー
-    text: {//テキストの形状を直接指定
-        style: {//天地中央に配置
+    easing: 'easeInOut',
+    duration: 2000,
+    strokeWidth: 0.2,
+    color: '#FF0',
+    trailWidth: 0.2,
+    trailColor: '#FFF',
+    text: {
+        style: {
             position: 'absolute',
             left: '50%',
             top: '50%',
             padding: '0',
-            margin: '-30px 0 0 0',//バーより上に配置
+            margin: '-30px 0 0 0',
             transform: 'translate(-50%,-50%)',
             'font-size': '3rem',
             color: '#FFF',
         },
-        autoStyleContainer: false //自動付与のスタイルを切る
+        autoStyleContainer: false 
     },
     step: function (state, bar) {
-        bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+        bar.setText(Math.round(bar.value() * 100) + ' %'); 
     }
 });
-//アニメーションスタート
-bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
-  $("#load").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+
+bar.animate(1.0, function () {
+  $("#load").delay(500).fadeOut(800);
 });
+
 
 
